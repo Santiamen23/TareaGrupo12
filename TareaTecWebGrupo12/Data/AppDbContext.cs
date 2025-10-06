d1f7d2fe1e6cec2044a1b4aa9f8915997a0e632e
Santiamen23 <santymenber@gmail.com>
2025-10-06 15:20:32 -0400

    fix:AppDbContext

--- TareaTecWebGrupo12/Data/AppDbContext.cs
+++ TareaTecWebGrupo12/Data/AppDbContext.cs
@@ -1,33 +1,33 @@
-﻿using System;
+
+using System;
 using Microsoft.EntityFrameworkCore;
 using TareaTecWebGrupo12.Models;
 
 namespace TareaTecWebGrupo12.Data
-{
-    public class AppDbContext : DbContext
-    {
-        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
-        {
-        }
-        public DbSet<Event> Events => Set<Event>();
-        public DbSet<Ticket> tickets => Set<Ticket>();
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
 
-            modelBuilder.Entity<Event>(e =>
-            
-            {
-                e.HasKey(x => x.Id);
-                e.Property(x => x.Title).IsRequired().HasMaxLength(200);
-                e.Property(x => x.Date).IsRequired();
-                e.Property(x => x.Capacity).IsRequired();
-                
-            });
-            modelBuilder.Entity<Ticket>(b =>
-            {
-                b.HasKey(x => x.Id);
-                b.Property(x => x.Notes).IsRequired().HasMaxLength(200);
-            });
-        }
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
     }
+    public DbSet<Event> Events => Set<Event>();
+    public DbSet<Ticket> tickets => Set<Ticket>();
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+
+        modelBuilder.Entity<Event>(e =>
+        {
+            e.HasKey(x => x.Id);
+            e.Property(x => x.Title).IsRequired().HasMaxLength(200);
+            e.Property(x => x.Date).IsRequired();
+            e.Property(x => x.Capacity).IsRequired();
+        });
+        modelBuilder.Entity<Ticket>(b =>
+        {
+            b.HasKey(x => x.Id);
+            b.Property(x => x.Notes).IsRequired().HasMaxLength(200);
+        });
+    }
+
+    } 
 }
