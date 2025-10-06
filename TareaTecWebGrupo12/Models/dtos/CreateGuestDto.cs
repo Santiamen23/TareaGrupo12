8c10ea06642cb3a213104c15124e06cd83c34c90
MariaRene20 <maria.soliz.s@ucb.edu.bo>
2025-10-05 21:43:45 -0400

    Add Guest model and DTO

---
+++ TareaTecWebGrupo12/Models/dtos/CreateGuestDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TareaTecWebGrupo12.Models.dtos
+{
+    public record CreateEventDto
+    {
+        [Required, StringLength(200)]
+        public string Title { get; set; } = string.Empty;
+
+        [Required]
+        public DateTime Date { get; set; }
+
+        [Range(1, int.MaxValue)]
+        public int Capacity { get; set; }
+    }
+}
