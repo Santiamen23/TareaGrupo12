3b4714b0275ab1e53fc9dbe8316b767025264cd6
MariaRene20 <maria.soliz.s@ucb.edu.bo>
2025-10-05 23:09:33 -0400

    Add repository layer for Guest entity

---
+++ TareaTecWebGrupo12/Repositories/GuestRepository.cs
@@ -0,0 +1,41 @@
+﻿using TareaTecWebGrupo12.Data;
+using TareaTecWebGrupo12.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace TareaTecWebGrupo12.Repositories
+{
+    public class EventRepository : IEventRepository
+    {
+        private readonly AppDbContext _context;
+        public EventRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task Add(Event anEvent)
+        {
+            await _context.Events.AddAsync(anEvent);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task Delete(Guid id)
+        {
+            var anEvent = await _context.Events.FirstOrDefaultAsync(x => x.Id == id);
+            if (anEvent != null)
+            {
+                _context.Events.Remove(anEvent);
+                await _context.SaveChangesAsync();
+            }
+        }
+
+        public async Task<IEnumerable<Event>> GetAll()
+        {
+            return await _context.Events.ToListAsync();
+        }
+
+        public async Task<Event?> GetById(Guid id)
+        {
+            return await _context.Events.FirstOrDefaultAsync(x => x.Id == id);
+        }
+    }
+}
---
+++ TareaTecWebGrupo12/Repositories/IGuestRepository.cs
@@ -0,0 +1,12 @@
+﻿using TareaTecWebGrupo12.Models;
+
+namespace TareaTecWebGrupo12.Repositories
+{
+    public interface IEventRepository
+    {
+        Task<IEnumerable<Event>> GetAll();
+        Task<Event?> GetById(Guid id);
+        Task Add(Event anEvent);
+        Task Delete(Guid id);
+    }
+}
